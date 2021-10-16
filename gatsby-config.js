1e84292feac34647054577114b43456ba51cc124
Thibaud <thibaudfaurevincent@gmail.com>
2021-10-16 21:15:23 +0200

    profil photo

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,7 +55,7 @@ module.exports = {
         background_color: `#d8dcdf`,
         theme_color: `#d8dcdf`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/profil.jpg`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-gatsby-cloud`,
